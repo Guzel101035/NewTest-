c478b92b16ee794f828e15cfe543bed0d9bcaba3
USER <cuxdi@yandex.ru>
2022-08-15 16:31:17 +0400

    задача14

---
+++ Test3/Program.cs
@@ -0,0 +1,18 @@
+﻿// на кратность на 7 и на 23.
+using static System.Console;
+Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+
+
+
+if(( number % 7 == 0 &&  number % 23 == 0))
+{
+    Console.WriteLine( "Да");
+}
+
+else
+{
+    Console.WriteLine("Нет");     
+}
