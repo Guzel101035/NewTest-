30e0c5f3237d3b1b9491bec900b41680177bff79
USER <cuxdi@yandex.ru>
2022-08-15 17:20:28 +0400

    задача16

---
+++ Test4/Program.cs
@@ -0,0 +1,22 @@
+﻿// на вход два числа и проверяет , является ли одно число квадратом другого.
+// 5, 25 -> да
+using static System.Console;
+Clear();
+
+Console.WriteLine("Inter number1");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inter number2");
+int number2= int.Parse(Console.ReadLine());
+
+
+if( number1 / number2 == number2 || number2 / number1 == number1 )
+{
+    Console.WriteLine( "Es");
+}
+
+else
+{
+    Console.WriteLine("No");     
+}
+
