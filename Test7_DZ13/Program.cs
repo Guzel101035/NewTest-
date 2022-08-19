64a19d2662531f7b06122a0ba1df2f9e4c69d1c8
USER <cuxdi@yandex.ru>
2022-08-19 16:18:48 +0400

    задача на чётность с тернарным

---
+++ Test7_DZ13/Program.cs
@@ -0,0 +1,8 @@
+﻿//13 продолжение
+using static System.Console;
+Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(ReadLine());
+
+string str = 32789.ToString();
+Console.WriteLine(str[2]);
---
+++ Test8__zadacha/Program.cs
@@ -0,0 +1,7 @@
+﻿// Тернарный оператор , задача : является ли число чётным?
+using static System.Console;
+Clear();
+WriteLine("Введите число ");
+int n = Convert.ToInt32(ReadLine());
+//string answer = n  % 2 == 0?"Yes": "No";
+WriteLine(n  % 2 == 0?"Yes": "No");
