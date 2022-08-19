faf64f8fd609fe2c31339d571eba1194f69a6e6a
USER <cuxdi@yandex.ru>
2022-08-19 21:56:27 +0400

    задача по четвертям

---
+++ Test8.4/Program.cs
@@ -0,0 +1,23 @@
+﻿// задача по координатам 
+using static System.Console;
+Clear();
+Write("Введите число x:");
+int x = Convert.ToInt32(ReadLine());
+Write("Введите y:");
+int y = Convert.ToInt32(ReadLine());
+if(x>0 && y>0)
+{
+    Write("первая четверть");
+}
+if(x>0 && y<0)
+{
+    Write("Вторая четверть");
+}
+if(x<0 && y<0)
+{
+    Write("Третья четверть");
+}
+if(x<0 && y>0)
+{
+   Write("Четвёртая");
+}
