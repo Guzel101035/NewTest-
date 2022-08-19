8aafc40d85abf05ec93cc1363c595dec037e9995
USER <cuxdi@yandex.ru>
2022-08-19 20:27:26 +0400

    задача на день недели

---
+++ Test8.2.DZ/Program.cs
@@ -0,0 +1,38 @@
+﻿// 
+using static System.Console;
+Clear();
+Write("Введите число a: ");
+int a = Convert.ToInt32(ReadLine());
+if(a==1)
+{
+    Console.WriteLine("Пн-рабочий ");
+}
+if(a==2)
+{
+    Console.WriteLine("Вт-рабочий");
+}
+if(a==3)
+{
+    Console.WriteLine("Ср-рабочий");
+}
+if(a==4)
+{
+    Console.WriteLine("Чт-рабочий");
+}
+if(a==5)
+{
+    Console.WriteLine("Пт-рабочий");
+}
+if(a==6)
+{
+    Console.WriteLine("Сб-выходной");
+}
+if(a==7)
+{
+    Console.WriteLine("Вс-Выходной");
+}
+
+if(a>7)
+{
+    Console.WriteLine("Не день недели");
+}
