85ad409b8554919ed5b7ef781ce365e3dc1370f5
USER <cuxdi@yandex.ru>
2022-08-19 21:11:59 +0400

    задача15 switch case

---
+++ Test8.3D.Z/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 15 на день недели и выходной(если пятидневка)
+using static System.Console;
+Clear();
+Write("Введите число a : ");
+int a = Convert.ToInt32(ReadLine());
+switch(a)
+{
+    case 1:
+    {
+        WriteLine("рабочий");
+        break ;
+    }        
+    case 2:
+    {
+        WriteLine("рабочий");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("рабочий");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("рабочий");
+        break;
+    }
+    case 5:
+    {
+        WriteLine("рабочий");
+        break;
+    }
+    case 6:
+    {
+        WriteLine("выходной");
+        break;
+    }
+    case 7:
+    {
+        WriteLine("выходной");
+        break;
+    }
+    default:
+    {
+        WriteLine("Это не день недели");
+        break;
+    }
+}    
