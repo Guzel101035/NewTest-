9924592e000ca61159dffe9e27f06c63772e294b
USER <cuxdi@yandex.ru>
2022-08-20 11:29:37 +0400

    задача 17 по четверти

---
+++ Test8.5.1/Program.cs
@@ -0,0 +1,35 @@
+﻿// вводим номер четверти , выводим данные x y 
+// 
+using static System.Console;
+Clear();
+Write("Введите число a:");
+int a = Convert.ToInt32(ReadLine());
+switch(a)
+{
+    case 1:
+    {
+        WriteLine("x>0 && y>0");
+        break ;
+    }        
+    case 2:
+    {
+        WriteLine("x>0 && y<0");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("x<0 && y<0");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("x<0 && y>0");
+        break;
+    }
+    default:
+    {
+        WriteLine("Это не номер четверти");
+        break;
+    }
+}            
+
